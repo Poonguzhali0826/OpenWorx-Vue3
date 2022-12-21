5a77e3549588fa6b61baf3a4f4db4d250af2b8ed
Poonguzhali0826 <poonguzhali.arumugam@twilightsoftwares.com>
2022-12-21 14:31:06 +0530

    feat: spinner changed

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -3,7 +3,6 @@
     <div class="row">
       <div class="col">
         <!-- <h2>Spinner Works!</h2> -->
-         <img src="/home/tlspc-054/Documents/workspace/openWorx-Vue 3/OpenWorx-Vue3/src/assets/spinner.gif" alt="" class="d-block m-auto">
       </div>
     </div>
   </div>
